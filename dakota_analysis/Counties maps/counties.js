7f07231a5cb5205b56268229e2b9041ded119f81
DakNewcomb <124754082+DakNewcomb@users.noreply.github.com>
2023-06-05 21:36:19 -0700

    started AZ county map

--- dakota_analysis/Counties maps/counties.js
+++ dakota_analysis/Counties maps/counties.js
@@ -1,7 +1,7 @@
 //Array to hold all AZ counties info
 azCounties = [];
 
-forEach row
+
 //Arrays to hold county info 
 apache =  [];
 cochise = [];
@@ -25,16 +25,16 @@ yuma = [];
 
 
 
-// // Creating the map object
-// let myMap = L.map("map", {
-//     center: [40.7128, -74.0059],
-//     zoom: 11
-//   });
+// Creating the map object
+let myMap = L.map("map", {
+    center: [34.4537, -112.0693],
+    zoom: 7
+  });
   
-//   // Adding the tile layer
-//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   }).addTo(myMap);
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
   
 //   // Use this link to get the GeoJSON data.
 //   let link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
