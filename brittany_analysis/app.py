42be928bda773bf2e14444acef32b9e7aba833dc
Brittany Brooks <Brit@Jesses-MacBook-Pro.local>
2023-06-07 18:19:31 -0700

    files

--- brittany_analysis/app.py
+++ brittany_analysis/app.py
@@ -10,13 +10,13 @@ from flask import Flask, jsonify, render_template
 #################################################
 engine = create_engine("sqlite:///data/hri.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(autoload_with=engine)
 
-# Save reference to the table
-County= Base.classes.county
+# # Save reference to the table
+# County= Base.classes.county
 
 #################################################
 # Flask Setup
@@ -28,10 +28,24 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+
 @app.route("/")
 def dashboard():
  
-#Create our dashboard
- session = Session(engine)
- 
+    #Create our dashboard
+    
+    #session = Session(engine)
+    return render_template("index.html")
 
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ brittany_analysis/static/js/app.js
@@ -0,0 +1,11 @@
+let test="test"
+var data = [
+    {
+      x: ['giraffes', 'orangutans', 'monkeys'],
+      y: [20, 14, 23],
+      type: 'bar'
+    }
+  ];
+  
+  Plotly.newPlot('plot', data);
+  
