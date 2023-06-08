ceee80473ad064f4c8bd44b38915c2aa06de1f02
DakNewcomb <124754082+DakNewcomb@users.noreply.github.com>
2023-06-07 22:00:19 -0700

    more mapping attempts

--- dakota_analysis/Counties maps/counties.js
+++ dakota_analysis/Counties maps/counties.js
@@ -1,53 +1,19 @@
+// Creating the map object
+let myMap = L.map("map", {
+  center: [34.4537, -112.0693],
+  zoom: 7
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
 //Array to hold all AZ counties info
 azCounties = [];
 
 //read geojson (reference https://www.freecodecamp.org/news/how-to-read-json-file-in-javascript/)
-fetch('county_shape.geojson')
-  .then((response) => response.json())
-  .then((geojson) => console.log(geojson));
+// fetch('AZ-04-arizona-counties.json')
+//   .then((response) => response.json())
+//   .then((geojson) => console.log(geojson))
 
-//loop like mod 15 day 2 activity 1
-
-//Arrays to hold county info 
-apache =  [];
-cochise = [];
-coconino = [];
-gila = [];
-graham = [];
-greenlee = [];
-laPaz = [];
-maricopa = [];
-mojave = [];
-navajo = [];
-pima = [];
-pinal = [];
-sataCruz = [];
-yavapai = [];
-yuma = [];
-
-
-
-
-
-
-
-// Creating the map object
-let myMap = L.map("map", {
-    center: [34.4537, -112.0693],
-    zoom: 7
-  });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-//   // Use this link to get the GeoJSON data.
-//   let link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
-  
-//   // Getting our GeoJSON data
-//   d3.json(link).then(function(data) {
-//     // Creating a GeoJSON layer with the retrieved data
-//     L.geoJson(data).addTo(myMap);
-//   });
-  
