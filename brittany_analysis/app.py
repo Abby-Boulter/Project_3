b76314ce74496c54022ce465349a4f8a3d40f783
Brittany Brooks <Brit@Jesses-MacBook-Pro.local>
2023-06-08 21:58:01 -0700

    flask

--- brittany_analysis/app.py
+++ brittany_analysis/app.py
@@ -2,8 +2,6 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-
 from flask import Flask, jsonify, render_template 
 
 #################################################
@@ -11,14 +9,19 @@ from flask import Flask, jsonify, render_template
 #################################################
 engine = create_engine("sqlite:///data/hri.sqlite")
 
-# # reflect an existing database into a new model
+#Reflect an existing database into a new model
 Base = automap_base()
-# # reflect the tables
+#Reflect the tables
 Base.prepare(autoload_with=engine)
 
-# # Save reference to the table
-#county= Base.classes.county
+#Save reference to the table
+#hri = Base.classes.hri
+#ed_visit = Base.classes.ed_visit
+#hospitalization = Base.classes.hospitalization
+#vulnerability = Base.classes.vulnerability
+#ed_visit_35 = Base.classes.ed_visit_35
 
+county= Base.classes.county
 #################################################
 # Flask Setup
 #################################################
@@ -28,12 +31,18 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-
 @app.route("/")
 def home():
       return render_template("index.html")
 
-   
+@app.route("/heat")
+def heat():
+   session = Session(engine)
+
+   results = session.query(county.hri).all()
+
+   session.close()
+ 
 
 
 if __name__ == "__main__":
