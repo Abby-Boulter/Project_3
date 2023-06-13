6bf5a6cb216e5f2650fc7fdcd1381fac45d772f5
Brittany Brooks <Brit@Jesses-MacBook-Pro.local>
2023-06-12 19:35:47 -0700

    files

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template 
 import numpy as np
+import json 
 #################################################
 # Database Setup
 #################################################
@@ -45,27 +46,28 @@ def dashboard():
 # HRI data tables have county, year, ed_rate, hosp_rate, vul_rate
 
     ## query county ed-rates for all years from SQLite database
-    results_ed_visit_35 = session.query(ed_visit_35.county, ed_visit_35.year, ed_visit_35.ed_rate).order_by(ed_visit_35.year.asc()).all()
+    results_ed_visit_35 = session.query(ed_visit_35.county, ed_visit_35.year, ed_visit_35.ed_rate_35).order_by(ed_visit_35.year.asc()).all()
 
     session.close()
 
         # Create a dictionary from the row data and append to a list of all_passengers
     all_ed_35 = []
-    for county, year, ed_rate in results_ed_visit_35:
+    for county, year, ed_rate_35 in results_ed_visit_35:
         ed_dict = {}
         ed_dict["county"] = county
         ed_dict["year"] = year
-        ed_dict["ed_rate"] = ed_rate
-        all_ed.append(ed_dict)
+        ed_dict["ed_rate"] = ed_rate_35
+        all_ed_35.append(ed_dict)
     
     return jsonify(all_ed_35)
 
- @app.route("/countiesjson")
+@app.route("/countiesjson")
 def test():
   with open('data/arizona-with-county-boundaries_1085.geojson','r') as gdata:
     geojson = json.load(gdata)
     print(geojson)
     return geojson
+
 
 
 if __name__ == "__main__":
